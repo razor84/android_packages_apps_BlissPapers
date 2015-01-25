426a2183f9e3041944c52bd0e7e3a2c99bbe451a
death2all110 <leeper.blake@gmail.com>
2015-01-25 17:08:52 -0600

    BlissPop Wallpapers for the BlissPop ROM by Team Bliss. http://blissroms.com
    Change-Id: I4dbbca90a44cc0a25dba0b34b2e029696f3b7022
    Signed-off-by: death2all110 <leeper.blake@gmail.com>

---
+++ com/death2all110/blisspapers/WallpaperCategory.java
@@ -0,0 +1,47 @@
+
+package com.death2all110.blisspapers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WallpaperCategory {
+
+    String id;
+    String name;
+    List<Wallpaper> wallpapers;
+
+    public WallpaperCategory(String i, String n) {
+        id = i;
+        name = n;
+        wallpapers = new ArrayList<Wallpaper>();
+    }
+
+    public void addWallpaper(Wallpaper wall) {
+        wallpapers.add(wall);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Wallpaper> getWallpapers() {
+        return wallpapers;
+    }
+
+    public void setWallpapers(List<Wallpaper> wallpapers) {
+        this.wallpapers = wallpapers;
+    }
+
+}
